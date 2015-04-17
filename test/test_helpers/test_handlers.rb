909e31017ac4c5de5bd991cc2407e4fbc757048b
Danny Guinther <dannyguinther@gmail.com>
2015-04-17 08:00:42 -0400

    Add test handlers test helper

---
+++ test/test_helpers/test_handlers.rb
@@ -0,0 +1,18 @@
+require "remotely_exceptional/matchers/delegate_matcher"
+require "remotely_exceptional/handlers/instance_handler"
+
+module RemotelyExceptional::Test
+  class BasicExceptionHandler
+    include RemotelyExceptional::Matchers::DelegateMatcher
+    include RemotelyExceptional::Handlers::InstanceHandler
+
+    class << self
+      attr_accessor :exception_class
+
+      def matcher_delegate
+        lambda { |ex| ex.is_a?(exception_class) }
+      end
+    end
+    self.exception_class = RuntimeError
+  end
+end
