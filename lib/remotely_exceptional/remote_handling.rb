d080c190e280b4f0eec08c0ba40106e526e6af7f
Danny Guinther <dannyguinther@gmail.com>
2015-04-18 10:06:21 -0400

    Shuffle some bits

--- lib/remotely_exceptional/remote_handling.rb
+++ lib/remotely_exceptional/remote_handling.rb
@@ -2,5 +2,7 @@ require "remotely_exceptional/exception_contexts/continue_raise_retry"
 
 module RemotelyExceptional::RemoteHandling
   extend Forwardable
-  def_delegator "RemotelyExceptional::ExceptionContexts::ContinueRaiseRetry", :context_exec, :remotely_exceptional
+  def_delegator "RemotelyExceptional::ExceptionContexts::ContinueRaiseRetry",
+    :context_exec,
+    :remotely_exceptional
 end
