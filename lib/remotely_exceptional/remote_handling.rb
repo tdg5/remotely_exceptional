c96dfd53a4131177827d4dbd3920170c4b543307
Danny Guinther <dannyguinther@gmail.com>
2015-04-22 12:39:53 -0400

    Replace Forwardable with method definitions

--- lib/remotely_exceptional/remote_handling.rb
+++ lib/remotely_exceptional/remote_handling.rb
@@ -1,6 +1,15 @@
 module RemotelyExceptional::RemoteHandling
-  extend Forwardable
-  def_delegator "RemotelyExceptional::ExceptionContext",
-    :execute,
-    :remotely_exceptional
+  def self.included(includer)
+    includer.extend(ClassMethods)
+  end
+
+  def remotely_exceptional(*args, &block)
+    self.class.remotely_exceptional(*args, &block)
+  end
+
+  module ClassMethods
+    def remotely_exceptional(*args, &block)
+      RemotelyExceptional::ExceptionContext.execute(*args, &block)
+    end
+  end
 end
