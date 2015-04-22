db8e30ac2f2cda145df34d562acddd06f0522310
Danny Guinther <dannyguinther@gmail.com>
2015-04-22 13:01:52 -0400

    Move version test to different context

--- test/unit/remotely_exceptional_test.rb
+++ test/unit/remotely_exceptional_test.rb
@@ -12,10 +12,13 @@ class RemotelyExceptionalTest < RemotelyExceptional::TestCase
       should "be defined" do
         assert_kind_of Module, subject
       end
+    end
+  end
 
-      should "have a version" do
-        assert_match(/\d+\.\d+\.\d+/, Subject::VERSION)
-      end
+  context Subject.name do
+    subject { Subject }
+    should "have a version" do
+      assert_match(/\d+\.\d+\.\d+/, Subject::VERSION)
     end
   end
 end
